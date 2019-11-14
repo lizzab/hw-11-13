e1ec090625e91e403b654840f9ba25693d5632c5
lizzab <Jjibber12>
2019-11-13 21:26:54 -0500

    Initial

---
+++ Commands.py
@@ -0,0 +1,13 @@
+# Ben Lizza
+# 11/13/19
+# This is the command module for Person.py I will use the methods in that file
+from Person import Person
+
+# Introduction to the Person (me)
+# Include first, middle, last name, sex, age, weight
+
+me = Person(Ben, C, Lizza, male, 18, 295)
+
+print(me.name())
+print(me.birthday())
+
